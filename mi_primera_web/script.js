f6cea342a66e1f9c8816e6a8fc787bf57bb7c274
san0314 <149741956+san0314@users.noreply.github.com>
2023-11-02 20:36:29 -0500

    Initial commit

---
+++ mi_primera_web/script.js
@@ -0,0 +1,11 @@
+alert("Hola este es mi Javascript"); 
+
+let nombre = "santiago" ; 
+
+const nombre1 = "orrego";
+
+var nombre2 = "rua";
+
+console.log (nombre);
+console.log (nombre1);
+console.log (nombre2);
